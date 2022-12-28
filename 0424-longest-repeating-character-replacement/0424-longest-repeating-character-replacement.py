e10d1d3ec1af56bf69b7541ee2073a1e5df02489
Ricardo Rumi <49548046+HumblePuff@users.noreply.github.com>
2022-12-28 20:27:53 -0300

    Time: 104 ms (97.29%), Space: 14 MB (57.70%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,10 +2,12 @@ from collections import defaultdict
 class Solution:
     def characterReplacement(self, s, k):
         dic = defaultdict(int)
-        start, end = 0, 0
-        for end in range(1, len(s)+1):
+        start = 0
+        End = 0
+        last_element=len(s)+1
+        for end in range(1, last_element):
             dic[s[end-1]] += 1
-            if end-start-max(dic.values()) > k: 
+            if end-start-max(dic.values()) > k:
                 dic[s[start]] -= 1
                 start += 1
-        return end-start
+        return end-start                            
