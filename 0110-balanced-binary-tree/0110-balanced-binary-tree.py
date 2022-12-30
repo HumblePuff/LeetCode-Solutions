812fccbb0495ec2527813e7115c7b5229016c534
Ricardo Rumi <49548046+HumblePuff@users.noreply.github.com>
2022-12-30 18:57:52 -0300

    Time: 51 ms (91.86%), Space: 18.7 MB (61.17%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,10 +1,8 @@
 class Solution:
-    def isBalanced(self, root: TreeNode, h=1) -> bool:
-        if not root: return h
-        l = self.isBalanced(root.left, h+1)
-        r = self.isBalanced(root.right, h+1)
-        #return abs(l-r) <= 1 and max(l, r)
-        if abs(l-r) <=1 :
-            return max(l,r)
-        else:
-            return False
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root: return 1
+        l = self.isBalanced(root.left)
+        if not l: return
+        r = self.isBalanced(root.right)
+        if not r: return
+        return abs(r - l) <= 1 and 1 + max(l, r)
