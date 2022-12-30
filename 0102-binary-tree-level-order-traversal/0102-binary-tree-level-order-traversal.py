1f2dd36d6aaf6aaa27b976fd702db87e3cbdfbd0
Ricardo Rumi <49548046+HumblePuff@users.noreply.github.com>
2022-12-30 12:50:04 -0300

    Time: 31 ms (96.84%), Space: 14.3 MB (51.32%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -3,5 +3,7 @@ class Solution:
         ans, level = [], [root]
         while root and level:
             ans.append([node.val for node in level])            
-            level = [kid for n in level for kid in (n.left, n.right) if kid]
+            level = [kid for n in level
+                     for kid in (n.left, n.right)
+                     if kid]
         return ans
